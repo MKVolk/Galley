318e6a113a4a45160a8e727824fd4594aa64ae23
Markos <163056970+MKVolk@users.noreply.github.com>
2026-07-15 02:22:26 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
-
 #!/data/data/com.termux/files/usr/bin/bash
 
 set -e
+
+GALLEY_DIR="$HOME/Galley"
+BASHRC="$HOME/.bashrc"
+
+echo "Removing existing Galley installation..."
+rm -rf "$GALLEY_DIR"
 
 echo "Updating packages..."
 pkg update -y
@@ -9,23 +14,28 @@ pkg update -y
 echo "Installing Git..."
 pkg install -y git
 
-echo "Removing existing Galley installation..."
-rm -rf ~/Galley
-
-echo "Cloning latest Galley..."
-git clone https://github.com/MKVolk/Galley.git ~/Galley
+echo "Cloning Galley..."
+git clone https://github.com/MKVolk/Galley.git "$GALLEY_DIR"
 
 echo "Making shell scripts executable..."
-find ~/Galley -name '*.sh' -exec chmod +x {} \;
+find "$GALLEY_DIR" -type f -name "*.sh" -exec chmod +x {} \;
 
-# Add alias if it doesn't already exist
-if ! grep -qxF "alias setupGLL='bash ~/Galley/setup.sh'" ~/.bashrc; then
-    echo "alias setupGLL='bash ~/Galley/setup.sh'" >> ~/.bashrc
+# Add alias only if it doesn't already exist
+if ! grep -qxF "alias setupGLL='bash \$HOME/Galley/setup.sh'" "$BASHRC"; then
+    echo "alias setupGLL='bash \$HOME/Galley/setup.sh'" >> "$BASHRC"
+    echo "Alias 'setupGLL' added to ~/.bashrc"
+else
+    echo "Alias 'setupGLL' already exists."
 fi
-
-# Reload shell configuration
-. ~/.bashrc
 
 echo
 echo "Galley has been updated successfully!"
-echo "Run 'setupGLL' to start the setup script."
+echo
+
+echo "To use the new alias, either run:"
+echo "  source ~/.bashrc"
+echo
+echo "or open a new Termux session."
+echo
+echo "Then start the installer with:"
+echo "  setupGLL"
